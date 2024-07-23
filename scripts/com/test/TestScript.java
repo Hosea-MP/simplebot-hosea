72b01e05822a60e883001ed0130e83cf85633f9e
Hosea-MP <76258414+Hosea-MP@users.noreply.github.com>
2024-07-23 15:54:35 -0500

    Initial commit

---
+++ scripts/com/test/TestScript.java
@@ -0,0 +1,51 @@
+package com.test;
+
+import simple.robot.script.Script;
+import simple.robot.api.ClientContext;
+import simple.hooks.wrappers.SimpleNpc;
+import simple.hooks.filters.SimpleNpcs;
+
+import java.awt.*;
+
+public class TestScript extends Script {
+
+    @Override
+    public void onExecute() {
+        // Initialization code if needed
+    }
+
+    @Override
+    public void onProcess() {
+        // Example user input for NPC name
+        String npcName = "Goblin"; // This should be replaced with actual user input
+
+        // Get the nearest NPC with the specified name
+        SimpleNpc nearestNpc = getNearestNpc(npcName);
+
+        if (nearestNpc != null) {
+            System.out.println("Nearest NPC: " + nearestNpc.getName() + " at " + nearestNpc.getLocation());
+        } else {
+            System.out.println("No NPC found with the name: " + npcName);
+        }
+    }
+
+    @Override
+    public void onTerminate() {
+        // Cleanup code if needed
+    }
+
+    @Override
+    public void onChatMessage(simple.hooks.simplebot.ChatMessage message) {
+        // Handle chat messages if needed
+    }
+
+    private SimpleNpc getNearestNpc(String name) {
+        SimpleNpcs npcs = ctx.npcs;
+        return npcs.filter(npc -> npc.getName().equalsIgnoreCase(name)).nearest().next();
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+
+    }
+}
---
+++ scripts/net/runelite/client/plugins/TestPlugin.java
@@ -0,0 +1,4 @@
+package net.runelite.client.plugins;
+
+public class TestPlugin {
+}
