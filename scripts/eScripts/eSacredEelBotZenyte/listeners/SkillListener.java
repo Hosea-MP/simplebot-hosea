7c2a42d42040f15cc78d5fea2a7bc541e016a663
Hosea-MP <76258414+Hosea-MP@users.noreply.github.com>
2024-07-25 10:11:27 -0500

    Initial commit

--- scripts/eScripts/eSacredEelBotZenyte/listeners/SkillListener.java
+++
@@ -1,8 +0,0 @@
-package eSacredEelBotZenyte.listeners;
-
-import simple.hooks.filters.SimpleSkills;
-
-public interface SkillListener {
-    void skillLevelAdded(SimpleSkills.Skills skill, int current, int previous, int gained);
-    void skillExperienceAdded(SimpleSkills.Skills skill, int current, int previous, int gained);
-}
